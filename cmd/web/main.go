c8f3f75a6337540b4a6499d568b764da335ddb6d
bl1nkker <nurovich14@gmail.com>
2024-04-14 19:25:30 +0500

    init final project

---
+++ cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+const webPort = "80"
+
+func main() {
+	// Connect to database
+
+	// Create sessions
+
+	// Create some channels
+
+	// Create wait group
+
+	// Set up the application config
+
+	// Set up mail
+
+	// Listen for web connections
+
+}
