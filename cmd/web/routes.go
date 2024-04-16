253f051d86dd6693be93bdaacbfd99f461ee6e60
bl1nkker <nurovich14@gmail.com>
2024-04-16 23:03:36 +0500

    added stub handlers

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -15,5 +15,12 @@ func (app *Config) routes() http.Handler {
 	mux.Use(middleware.Recoverer)
 	mux.Use(app.SessionLoad)
 	mux.Get("/", app.HomePage)
+
+	mux.Get("/login", app.LoginPage)
+	mux.Post("/login", app.PostLoginPage)
+	mux.Get("/logout", app.Logout)
+	mux.Get("/register", app.RegisterPage)
+	mux.Post("/register", app.PostRegisterPage)
+	mux.Post("/activate", app.ActivateAccount)
 	return mux
 }
