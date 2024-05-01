7ab66ed432760e5482e533c5be617b4dc4f0f58f
bl1nkker <nurovich14@gmail.com>
2024-05-01 15:58:17 +0500

    added auth middleware + retrieved plan data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
 	"working-with-concurrency-final-project/data"
 )
 
@@ -152,11 +153,24 @@ func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {
 
 func (app *Config) SubscribeToPlan(w http.ResponseWriter, r *http.Request) {
 	// get id of a plan that is choosen
+	id := r.URL.Query().Get("id")
+	planID, _ := strconv.Atoi(id)
 
 	// get the plan from the db
+	plan, err := app.Models.Plan.GetOne(planID)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Unable to find plan")
+		http.Redirect(w, r, "/members/plans", http.StatusSeeOther)
+		return
+	}
 
 	// get the user from the session
-
+	user, ok := app.Session.Get(r.Context(), "user").(data.User)
+	if !ok {
+		app.Session.Put(r.Context(), "error", "Login first!")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
 	// generate an invoice
 
 	// send an email with the invoice attached
@@ -171,11 +185,6 @@ func (app *Config) SubscribeToPlan(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Config) ChooseSubscription(w http.ResponseWriter, r *http.Request) {
-	if !app.Session.Exists(r.Context(), "userID") {
-		app.Session.Put(r.Context(), "warning", "You must log in to see this page")
-		http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
-		return
-	}
 	plans, err := app.Models.Plan.GetAll()
 	if err != nil {
 		app.ErrorLog.Println(err)
--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -7,3 +7,14 @@ func (app *Config) SessionLoad(next http.Handler) http.Handler {
 	// LoadAndSave performs tasks related to loading session data from the incoming request and saving it back after the request is handled.
 	return app.Session.LoadAndSave(next)
 }
+
+func (app *Config) Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !app.Session.Exists(r.Context(), "userID") {
+			app.Session.Put(r.Context(), "warning", "You must log in to see this page")
+			http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
